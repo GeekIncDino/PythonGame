941a349ceb29ca88aee4a43fb3ec5eededeca6aa
GeekIncDino <geekincdino@gmail.com>
2016-03-14 12:07:51 -0600

    pygame.py
    Added Stuff

--- pygame.py
+++ pygame.py
@@ -1,18 +1,51 @@
+#Platform game with a twist
+#I stole a couple lines from pygame.org, but I labeled them all
+#
+#Released under the  GNU General Public License
+
 import pygame as pg
 
-pg.init()
-screen = pg.display.set_mode((400, 300))
-done = False
+pg.init()				#From pygame.org
+screen = pg.display.set_mode((150, 50))	#From pygame.org, modified
+done = False 				#From pygame.org
 
-while not done:
-        for event in pg.event.get():
-                if event.type == pg.QUIT:
-                        done = True
-        
-        pg.display.flip()
+bkgrd = pg.Surface(screen.get_size())
+bkgrd = bkgrd.convert()
+bkgrd.fill((10, 15, 200))
 
-def gameStart:
-		print "Starting Game"
+font = pg.font.SysFont(Arial, 12)
+
+def checkGood:
+	pg.key.get.pressed()
+	if 
+def runLVL1:
+
+def runLVL2:
+	
+def runLVL3:
+
+def runLVL4:
+	
+def runLVL5:
+
+
+def gameStart:									
+	print "Starting Game %n"
+	print "Choose a level: 1 2 3 4 5 %n"
+	raw_input()
+	if raw_input() == 1:
+		runLVL1
+	if raw_input() == 2:
+		runLVL3
+	if raw_input() == 3:
+		runLVL3
+	if raw_input() == 4:
+		runLVL4
+	if raw_input() == 5:
+		runLVL5
+	else:
+		print "Your only options are 1 through 5"
+		gameStart
 
 if event.type == pg.KEYDOWN and event.key == pg.K_ENTER:
 	gameStart
@@ -28,6 +61,3 @@ if event.type == pg.KEYDOWN and event.key == pg.K_UP:
         
 if event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
 	movedown
-
-
-
